6c784a80946fe0f8d0354413b53eca69ba17b7ce
Halley Souza <souza.halley@gmail.com>
2019-10-23 12:40:27 +0100

    Template debug

--- models/multiquestion.py
+++ models/multiquestion.py
@@ -1,14 +1,14 @@
-# from __main__ import app
+from __main__ import app
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import uuid
 
 #### DEBUG PURPOSES
-from flask import Flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cxYFqST8YcuK6wZV6Aoy-P_JBVYXBMSUlLBV2WsCKWQ'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lynko:lynko@localhost/academy'
+# from flask import Flask
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'cxYFqST8YcuK6wZV6Aoy-P_JBVYXBMSUlLBV2WsCKWQ'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lynko:lynko@localhost/academy'
 
 db = SQLAlchemy(app)
 
@@ -20,7 +20,7 @@ class Types(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     type = db.Column(db.String(80), nullable=False)
     typename = db.Column(db.String(80), nullable=False)
-    question = db.relationship('Questions', secondary='multiquestions.relationtypes', backref=db.backref('mtypes', lazy='dynamic'))
+    question = db.relationship('Questions', secondary='multiquestions.relationtypes', backref=db.backref('types', lazy='dynamic'))
 
 relationtypes = db.Table('relationtypes', db.Model.metadata,
     db.Column('id_question', db.Integer, db.ForeignKey('multiquestions.questions.id')),
@@ -32,17 +32,36 @@ class Tags(db.Model):
     __table_args__ = {'schema' : 'multiquestions'}
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     tagname = db.Column(db.String(80), unique=True, nullable=False)
-    question = db.relationship('Questions', secondary='multiquestions.relationtags', backref=db.backref('mquestions', lazy='dynamic'))
+    question = db.relationship('Questions', secondary='multiquestions.relationtags', backref=db.backref('tags', lazy='dynamic'))
 
     def serialize(self):
+        output = []
+        for q in self.question:
+            mq = {}
+            mq['uid'] = q.uid  
+            output.append(mq)
+
         return {
             'tagname' : self.tagname,
-            'question' : self.question
+            'question' : output
         }
 
 relationtags = db.Table('relationtags', db.Model.metadata,
     db.Column('id_question', db.Integer, db.ForeignKey('multiquestions.questions.id')),
     db.Column('id_tag', db.Integer, db.ForeignKey('multiquestions.tags.id')),
+    schema='multiquestions')
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    __table_args__ = {'schema' : 'multiquestions'}
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    filetype = db.Column(db.String(32))
+    data = db.Column(db.LargeBinary)
+    question = db.relationship('Questions', secondary='multiquestions.relationmedia', backref=db.backref('medias', lazy='dynamic'))
+
+relationmedia = db.Table('relationmedia', db.Model.metadata,
+    db.Column('id_question', db.Integer, db.ForeignKey('multiquestions.questions.id')),
+    db.Column('id_media', db.Integer, db.ForeignKey('multiquestions.media.id')),
     schema='multiquestions')
 
 class Questions(db.Model):
@@ -58,15 +77,16 @@ class Questions(db.Model):
     option3 = db.Column(db.Text, nullable=False)
     option4 = db.Column(db.Text, nullable=False)
     score = db.Column(db.Integer, nullable=False)
+    correct = db.Column(db.Text, nullable=False)
 
     def serialize(self):
         return {
             'uuid' : self.uid,
+            'creation' : self.creation,
             'question' : self.question,
             'options' : [self.option1, self.option2, self.option3, self.option4],
-            'score' : self.score,
-            'tags' : self.tagname
-            
+            'correct' : self.correct,
+            'score' : self.score
         }
 
 # db.drop_all()
@@ -78,7 +98,8 @@ class Questions(db.Model):
 #     option2 = 'Answer2',
 #     option3 = 'Answer3',
 #     option4 = 'Answer4',
-#     score = 1
+#     score = 1,
+#     correct = 'option2'
 # )
 # list_tags = ["teste", "office", "glass", "dog", "tag"]
 
